eab5d63d15675536eb3498b4bb195f6bbc4c66e0
sagormahtab <sagormahtab@gmail.com>
2020-03-28 13:02:17 +0600

    lightoj-1001 done

--- lightoj-1001.cpp
+++ lightoj-1001.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-  
+  int T, item;
+  cin>>T;
+  for(int i = 0; i < T; i++){
+    cin>>item;
+    if(item <= 10)
+      cout<<"0"<<" "<<item<<endl;
+    else
+      cout<<item-10<<" "<<"10"<<endl;
+  }
   return 0;
 }
