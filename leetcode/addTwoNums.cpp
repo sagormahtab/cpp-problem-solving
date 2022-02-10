5d380325b0c10c4c7d8951b0b99d69937f46af19
sagormahtab <sagormahtab@gmail.com>
2022-02-10 23:49:54 +0600

    simulated basic linked list

--- leetcode/addTwoNums.cpp
+++ leetcode/addTwoNums.cpp
@@ -1,12 +1,42 @@
 #include <iostream>
+using namespace std;
 
-struct Node
-{
-  int data;
-  struct Node *next;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+  cout << l1 -> next -> next -> val;
+  return l1;
 };
 
 int main()
 {
+  struct ListNode* l1 = (struct ListNode*)malloc(3 * sizeof(struct ListNode*));
+  struct ListNode* l2 = (struct ListNode*)malloc(3 * sizeof(struct ListNode*));
+
+  l1 -> val = 2;
+  l1 -> next = (l1 + 1);
+
+  (l1 + 1) -> val = 4;
+  (l1 + 1) -> next = (l1 + 2);
+
+  (l1 + 2) -> val = 3;
+  (l1 + 2) -> next = NULL;
+
+  l2 -> val = 5;
+  l2 -> next = (l2 + 1);
+
+  (l2 + 1) -> val = 6;
+  (l2 + 1) -> next = (l2 + 2);
+
+  (l2 + 2) -> val = 4;
+  (l2 + 2) -> next = NULL;
+
+  struct ListNode* result = addTwoNumbers(l1, l2);
   return 0;
 }
