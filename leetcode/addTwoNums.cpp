380314b4ecc7987af1fcc8adc0847cbf0fa4e864
sagormahtab <sagormahtab@gmail.com>
2022-02-11 23:01:50 +0600

    fuck cpp, fuck pointers, fuck malloc

--- leetcode/addTwoNums.cpp
+++ leetcode/addTwoNums.cpp
@@ -1,24 +1,69 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-struct ListNode {
+typedef struct ListNode {
     int val;
     ListNode *next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+} ListNode;
 
 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-  cout << l1 -> next -> next -> val;
+  int i = 0, sum = 0, unit = 1;
+  // ListNode *l3 = new ListNode;
+  // struct ListNode* l3 = (struct ListNode*)malloc(3 * sizeof(struct ListNode*));
+
+  // while (l1 != NULL) {
+  //   cout << l1 -> val;
+  //   l1 = l1 -> next;
+  // }
+
+  while(l1 != NULL || l2 != NULL) {
+    int localSum = 0;
+    if (l1 -> val == NULL) {
+      localSum += l2 -> val;
+    }
+
+    if (l2 -> val == NULL) {
+      localSum += l1 -> val;
+    }
+    localSum += l1 -> val + l2 -> val;
+
+    cout << l1 -> val << setw(5) << l2 -> val << setw(5) << localSum << endl;
+    sum = sum + localSum * unit;
+  
+    l1 = l1 -> next;
+    l2 = l2 -> next;
+    unit *= 10;
+  }
+  cout << sum;
+  string sumStr = to_string(sum);
+  ListNode* l3 = (ListNode*)malloc(sizeof(ListNode*));
+  cout << "bruh"<< sumStr << endl;
+//   for(int i = sumStr.length(), j = 0; i > -1; i--, j++) {
+//     (l3 + j) -> val = (int) sumStr[i];
+//     if (i == 0) {
+//       (l3 + j) -> next = NULL;
+//     } else {
+//       (l3 + j) -> next = (l3 + j + 1);
+//     }
+//   }
+// cout << l3 -> val;
+//   while (l3 != NULL) {
+//     cout << l3 -> val;
+//     l3 = l3 -> next;
+//   }
   return l1;
 };
 
 int main()
 {
-  struct ListNode* l1 = (struct ListNode*)malloc(3 * sizeof(struct ListNode*));
-  struct ListNode* l2 = (struct ListNode*)malloc(3 * sizeof(struct ListNode*));
+  ListNode* l1 = (ListNode*)malloc(4 * sizeof(ListNode*));
+  ListNode* l2 = (ListNode*)malloc(4 * sizeof(ListNode*));
 
+  cout << l1 << endl << l2 << endl;
   l1 -> val = 2;
   l1 -> next = (l1 + 1);
 
