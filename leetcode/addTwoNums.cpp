57c5a105bfdb63fe2af7aaa22fbb8e99fb4a491f
sagormahtab <sagormahtab@gmail.com>
2022-02-12 16:11:07 +0600

    addTwoNumber method modified

--- leetcode/addTwoNums.cpp
+++ leetcode/addTwoNums.cpp
@@ -2,86 +2,112 @@
 #include <iomanip>
 using namespace std;
 
-typedef struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+typedef struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
 } ListNode;
 
-ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+{
   int i = 0, sum = 0, unit = 1;
-  // ListNode *l3 = new ListNode;
-  // struct ListNode* l3 = (struct ListNode*)malloc(3 * sizeof(struct ListNode*));
+  // copy of the initial pointer so that the linked list is not destroyed
+  ListNode *p = l1;
+  ListNode *q = l2;
 
-  // while (l1 != NULL) {
-  //   cout << l1 -> val;
-  //   l1 = l1 -> next;
-  // }
+  ListNode *l3 = new ListNode;
+  ListNode *head = l3;
 
-  while(l1 != NULL || l2 != NULL) {
+  while (p != NULL || q != NULL)
+  {
     int localSum = 0;
-    if (l1 -> val == NULL) {
-      localSum += l2 -> val;
+    if (p == NULL)
+    {
+      localSum += q->val;
     }
 
-    if (l2 -> val == NULL) {
-      localSum += l1 -> val;
+    if (q == NULL)
+    {
+      localSum += p->val;
     }
-    localSum += l1 -> val + l2 -> val;
+    localSum += p->val + q->val;
 
-    cout << l1 -> val << setw(5) << l2 -> val << setw(5) << localSum << endl;
+    cout << p->val << setw(5) << q->val << setw(5) << localSum << endl;
     sum = sum + localSum * unit;
-  
-    l1 = l1 -> next;
-    l2 = l2 -> next;
+
+    p = p->next;
+    q = q->next;
     unit *= 10;
   }
   cout << sum;
-  string sumStr = to_string(sum);
-  ListNode* l3 = (ListNode*)malloc(sizeof(ListNode*));
-  cout << "bruh"<< sumStr << endl;
-//   for(int i = sumStr.length(), j = 0; i > -1; i--, j++) {
-//     (l3 + j) -> val = (int) sumStr[i];
-//     if (i == 0) {
-//       (l3 + j) -> next = NULL;
-//     } else {
-//       (l3 + j) -> next = (l3 + j + 1);
-//     }
-//   }
-// cout << l3 -> val;
-//   while (l3 != NULL) {
-//     cout << l3 -> val;
-//     l3 = l3 -> next;
-//   }
+
+  // string sumStr = to_string(sum);
+  // cout << "bruh" << sumStr << endl;
+  //   for(int i = sumStr.length(), j = 0; i > -1; i--, j++) {
+  //     (l3 + j) -> val = (int) sumStr[i];
+  //     if (i == 0) {
+  //       (l3 + j) -> next = NULL;
+  //     } else {
+  //       (l3 + j) -> next = (l3 + j + 1);
+  //     }
+  //   }
+  // cout << l3 -> val;
+  //   while (l3 != NULL) {
+  //     cout << l3 -> val;
+  //     l3 = l3 -> next;
+  //   }
   return l1;
 };
 
 int main()
 {
-  ListNode* l1 = (ListNode*)malloc(4 * sizeof(ListNode*));
-  ListNode* l2 = (ListNode*)malloc(4 * sizeof(ListNode*));
+  // Defining Head
+  ListNode *l1Head;
+  ListNode *l2Head;
 
-  cout << l1 << endl << l2 << endl;
-  l1 -> val = 2;
-  l1 -> next = (l1 + 1);
+  // Initializing nodes with NULL
+  ListNode *l1One = NULL;
+  ListNode *l1Two = NULL;
+  ListNode *l1Three = NULL;
 
-  (l1 + 1) -> val = 4;
-  (l1 + 1) -> next = (l1 + 2);
+  ListNode *l2One = NULL;
+  ListNode *l2Two = NULL;
+  ListNode *l2Three = NULL;
 
-  (l1 + 2) -> val = 3;
-  (l1 + 2) -> next = NULL;
+  // allocate nodes in the heap
+  l1One = new ListNode;
+  l1Two = new ListNode;
+  l1Three = new ListNode;
 
-  l2 -> val = 5;
-  l2 -> next = (l2 + 1);
+  l2One = new ListNode;
+  l2Two = new ListNode;
+  l2Three = new ListNode;
 
-  (l2 + 1) -> val = 6;
-  (l2 + 1) -> next = (l2 + 2);
+  // Assign val values
+  l1One->val = 2;
+  l1Two->val = 4;
+  l1Three->val = 3;
 
-  (l2 + 2) -> val = 4;
-  (l2 + 2) -> next = NULL;
+  l2One->val = 5;
+  l2Two->val = 6;
+  l2Three->val = 4;
 
-  struct ListNode* result = addTwoNumbers(l1, l2);
+  // Connect Nodes
+  l1One->next = l1Two;
+  l1Two->next = l1Three;
+  l1Three->next = NULL;
+
+  l2One->next = l2Two;
+  l2Two->next = l2Three;
+  l2Three->next = NULL;
+
+  // Connect with head
+  l1Head = l1One;
+  l2Head = l2One;
+
+  ListNode *result = addTwoNumbers(l1Head, l2Head);
   return 0;
 }
