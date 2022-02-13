0e66592e364212ab5bb184e8eb10f13609fc0c98
sagormahtab <sagormahtab@gmail.com>
2022-02-13 12:41:36 +0600

    addTwoNum solved heh

--- leetcode/addTwoNums.cpp
+++ leetcode/addTwoNums.cpp
@@ -15,46 +15,53 @@ typedef struct ListNode
 
 ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
 {
-  long long sum = 0;
-  int unit = 1;
+  int unit = 1, sum = 0, carry = 0;
   // copy of the initial pointer so that the linked list is not destroyed
   ListNode *p = l1;
   ListNode *q = l2;
 
   ListNode *l3 = new ListNode;
   ListNode *head = l3;
+  ListNode *r = head;
 
   while (p != NULL || q != NULL)
   {
     int localSum = 0;
-    if (p == NULL) {
-      localSum += q->val;
-      q = q -> next;
-    } else if (q == NULL) {
-      localSum += p->val;
-      p = p -> next;
-    } else {
-      localSum += p->val + q->val;
-      p = p->next;
+    if (p == NULL)
+    {
+      localSum = q->val + carry;
+      r->val = localSum % 10;
       q = q->next;
     }
-
-    sum = sum + localSum * unit;
-    unit *= 10;
-  }
-
-  string sumStr = to_string(sum);
-  // assigning the reverse string to a new linked list
-  for(int i = sumStr.length() - 1; i > -1; i--) {
-    // converting character to integer
-    l3 -> val = sumStr[i] - 48;
-    if (i == 0) {
-      l3 -> next = NULL;
-      break;
-    } else {
-      l3 -> next = new ListNode(sumStr[i] - 48);
-      l3 = l3 -> next;
+    else if (q == NULL)
+    {
+      localSum = p->val + carry;
+      r->val = localSum % 10;
+      p = p->next;
     }
+    else
+    {
+      localSum = (p->val + q->val + carry);
+      p = p->next;
+      q = q->next;
+      r->val = localSum % 10;
+    }
+
+    carry = localSum / 10;
+
+    if (p == NULL && q == NULL)
+    {
+      if (carry == 1)
+      {
+        r->next = new ListNode(carry);
+        break;
+      }
+      r->next = NULL;
+      break;
+    }
+
+    r->next = new ListNode;
+    r = r->next;
   }
 
   return head;
@@ -62,22 +69,24 @@ ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
 
 int main()
 {
-  vector<int> list1 {9};
-  vector<int> list2 {1,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9};
+  vector<int> list1{2, 4};
+  vector<int> list2{5, 6, 9};
 
   // Generating l1 linked list
   ListNode *l1 = new ListNode;
   ListNode *l1Head = l1;
   ListNode *p = l1Head;
 
-  for (int i = 0; i < list1.size(); i++) {
-    p -> val = list1[i];
-    if (i == list1.size() - 1) {
-      p -> next = NULL;
+  for (int i = 0; i < list1.size(); i++)
+  {
+    p->val = list1[i];
+    if (i == list1.size() - 1)
+    {
+      p->next = NULL;
       break;
     }
-    p -> next = new ListNode(list1[i + 1]);
-    p = p -> next;
+    p->next = new ListNode(list1[i + 1]);
+    p = p->next;
   }
 
   // Generating l2 linked list
@@ -85,20 +94,23 @@ int main()
   ListNode *l2Head = l2;
   ListNode *q = l2Head;
 
-  for (int i = 0; i < list2.size(); i++) {
-    q -> val = list2[i];
-    if (i == list2.size() - 1) {
-      q -> next = NULL;
+  for (int i = 0; i < list2.size(); i++)
+  {
+    q->val = list2[i];
+    if (i == list2.size() - 1)
+    {
+      q->next = NULL;
       break;
     }
-    q -> next = new ListNode(list2[i + 1]);
-    q = q -> next;
+    q->next = new ListNode(list2[i + 1]);
+    q = q->next;
   }
 
   ListNode *result = addTwoNumbers(l1Head, l2Head);
-  while (result != NULL) {
-    cout << result -> val;
-    result = result -> next;
+  while (result != NULL)
+  {
+    cout << result->val;
+    result = result->next;
   }
   return 0;
 }
