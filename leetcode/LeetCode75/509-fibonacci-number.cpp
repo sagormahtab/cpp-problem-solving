122076fffe3f5daf70aa8e1f7d8d04ae7a9b3f4c
sagormahtab <sagormahtab@gmail.com>
2022-07-12 16:27:19 +0600

    509-fibonacci-number

---
+++ leetcode/LeetCode75/509-fibonacci-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+  if (n == 0 || n == 1)
+    return n;
+  return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+  int n = 5;
+  int result = fib(n);
+  cout << result;
+  return 0;
+}
