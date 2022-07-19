d243d198247922c5e8c93389dca0f1afcedc5759
sagormahtab <sagormahtab@gmail.com>
2022-07-19 14:07:10 +0600

    724-find-pivot-index updated

--- leetcode/LeetCode75/724-find-pivot-index.cpp
+++ leetcode/LeetCode75/724-find-pivot-index.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
+#include <numeric>
 #include <vector>
 using namespace std;
 
-int pivotIndex(vector<int>& nums) {
-    int index = -1;
+int pivotIndex(vector<int> &nums)
+{
     int leftSum = 0;
     int rightSum = 0;
 
-    for(int i = 0; i < nums.size(); i++) {
-        rightSum += nums[i];
-    }
-    for(int i = 0; i < nums.size(); i++) {
-        if (i != 0) {
-            leftSum += nums[i-1];
-        }
+    rightSum = accumulate(nums.begin(), nums.end(), 0);
+    for (int i = 0; i < nums.size(); i++)
+    {
         rightSum -= nums[i];
-        if(leftSum == rightSum) {
-            index = i;
-            break;
+        if (leftSum == rightSum)
+        {
+            return i;
         }
+        leftSum += nums[i];
     }
-    return index;
+    return -1;
 }
 
-int main() {
+int main()
+{
     int index;
-    vector<int> nums = {1,7,3,6,5,6};
+    vector<int> nums = {1, 7, 3, 6, 5, 6};
     index = pivotIndex(nums);
-    cout << index <<endl;
+    cout << index << endl;
     return 0;
 }
